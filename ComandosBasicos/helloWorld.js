9b5014978c1d2f6515df5f5c11776fc4b5a10fd3
Marcio Pereira <marcio.pereira@shift.com.br>
2021-04-20 22:19:21 -0300

    Comandos básicos e helloWorld

---
+++ ComandosBasicos/helloWorld.js
@@ -0,0 +1,13 @@
+helloWorld = 'Hello World';
+
+function escreverOlaMundo() {
+    document.getElementById('helloWorld').innerHTML = helloWorld;
+}
+
+function somarDoisNumeros() {
+    let numero1, numero2;
+    numero1 = 5;
+    numero2 = 20.5;
+
+    return numero1 + numero2;
+}
